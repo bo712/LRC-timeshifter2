fe61c12bbdaa1aba740429f834e36855e61e8a7e
Konstantin Babakov <linuksoid@gmail.com>
2018-08-13 00:04:42 +0300

    try to read

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("/home/kot/IdeaProjects/LRC-timeshifter2/src/notes2.txt");
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
