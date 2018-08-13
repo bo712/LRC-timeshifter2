f76aad31f51e664c25930c2ce458173f76a3a356
Konstantin Babakov <linuksoid@gmail.com>
2018-08-13 07:42:54 +0300

    try to read

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите путь к файлу lrc: ");
+//        String name = in.nextLine();
+        String name = "/home/kot/IdeaProjects/LRC-timeshifter2/src/notes2.txt";
+
+        System.out.print("Введите смещение: ");
+        String shift = in.nextLine();
+
         try {
-            FileReader reader = new FileReader("/home/kot/IdeaProjects/LRC-timeshifter2/src/notes2.txt");
+            FileReader reader = new FileReader(name);
         } catch(IOException ex){
-            System.out.println(ex.getMessage());
+            System.err.println(ex.getMessage());
         }
     }
 
