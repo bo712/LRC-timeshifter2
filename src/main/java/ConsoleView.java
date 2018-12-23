a5b8bd5d6dc74984a6d223dce5d8b3f47d8f56c5
kot <linuksoid@gmail.com>
2018-12-23 18:56:18 +0300

    System.out.println -> log.info

--- src/main/java/ConsoleView.java
+++ src/main/java/ConsoleView.java
@@ -1,21 +1,25 @@
+import org.slf4j.Logger;
+
 class ConsoleView {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(ConsoleView.class);
 
     private ConsoleView() {
     }
 
     static void enteringInputFile(){
-        System.out.print("Введите путь к файлу lrc: ");
+        log.info("Введите путь к файлу lrc: ");
     }
 
     static void enteringShiftValue(){
-        System.out.print("Введите смещение в формате \"+01:10.25\": ");
+        log.info("Введите смещение в формате \"+01:10.25\": ");
     }
 
     static void readFileError() {
-        System.err.println("Проблемы с чтением исходного файла =(");
+        log.error("Проблемы с чтением исходного файла =(");
     }
 
     static void writeFileError() {
-        System.err.println("Проблемы с записью результата в файл =(");
+        log.error("Проблемы с записью результата в файл =(");
     }
 }
